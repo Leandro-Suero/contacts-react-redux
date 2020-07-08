f25ab65ddc71a667ec16558118b85263005c76ff
Leandro-Suero <ing.leandrosuero@gmail.com>
2020-07-08 20:18:41 -0300

    Added update with redux

--- src/actions/contactActions.js
+++ src/actions/contactActions.js
@@ -1,19 +1,53 @@
-import { GET_CONTACTS, DELETE_CONTACT, ADD_CONTACTS } from "./types";
+import {
+    GET_CONTACTS,
+    DELETE_CONTACT,
+    ADD_CONTACTS,
+    GET_CONTACT,
+    UPDATE_CONTACT,
+} from "./types";
+import Axios from "axios";
 
-export const getContacts = () => {
-    return {
+export const getContacts = () => async (dispatch) => {
+    const res = await Axios.get("http://jsonplaceholder.typicode.com/users");
+    dispatch({
         type: GET_CONTACTS,
-    };
+        payload: res.data,
+    });
 };
-export const deleteContact = (id) => {
-    return {
+export const getContact = (id) => async (dispatch) => {
+    const res = await Axios.get(
+        `http://jsonplaceholder.typicode.com/users/${id}`
+    );
+    dispatch({
+        type: GET_CONTACT,
+        payload: res.data,
+    });
+};
+export const deleteContact = (id) => async (dispatch) => {
+    await Axios.delete(`http://jsonplaceholder.typicode.com/users/${id}`);
+    dispatch({
         type: DELETE_CONTACT,
         payload: id,
-    };
+    });
 };
-export const addContacts = (contact) => {
-    return {
+export const addContacts = (contact) => async (dispatch) => {
+    const res = await Axios.post(
+        "http://jsonplaceholder.typicode.com/users",
+        contact
+    );
+    dispatch({
         type: ADD_CONTACTS,
-        payload: contact,
-    };
+        payload: res.data,
+    });
+};
+export const updateContact = (contact) => async (dispatch) => {
+    const res = await Axios.put(
+        `http://jsonplaceholder.typicode.com/users/${contact.id}`,
+        contact
+    );
+    console.log("contact updated: ", res.data);
+    dispatch({
+        type: UPDATE_CONTACT,
+        payload: res.data,
+    });
 };
